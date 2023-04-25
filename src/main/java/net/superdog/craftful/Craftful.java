965ef625e5d2f574358c400aec5ae0c35122718d
Super1166 <110970817+Super1166@users.noreply.github.com>
2023-04-25 16:14:21 +0300

    tomato seeds jungle temple loot table

--- src/main/java/net/superdog/craftful/Craftful.java
+++ src/main/java/net/superdog/craftful/Craftful.java
@@ -2,6 +2,10 @@ package net.superdog.craftful;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.util.Identifier;
 import net.superdog.craftful.block.ModBlocks;
 import net.superdog.craftful.item.ModItems;
 import org.slf4j.Logger;
@@ -12,8 +16,20 @@ public class Craftful implements ModInitializer {
 	public static final String MOD_ID = "craftful";
 	public static final Logger LOGGER = LoggerFactory.getLogger("craftful");
 
+	// No magic constants!
+	private static final Identifier JUNGLE_TEMPLE_CHEST_LOOT_TABLE_ID = new Identifier("minecraft", "chests/jungle_temple");
+
 	@Override
 	public void onInitialize() {
+
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin() && JUNGLE_TEMPLE_CHEST_LOOT_TABLE_ID.equals(id)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.with(ItemEntry.builder(ModItems.TOMATO_SEEDS));
+				tableBuilder.pool(poolBuilder);
+			}
+		});
+
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 	}
