e8a2201c3976b2259eee6f43ca1f6ec6fed8778e
Super1166 <110970817+Super1166@users.noreply.github.com>
2023-04-27 23:42:12 +0300

    Limestone, Limestone bricks and Limestone pillars blocks

--- src/main/java/net/superdog/craftful/block/ModBlocks.java
+++ src/main/java/net/superdog/craftful/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.sound.BlockSoundGroup;
 import net.superdog.craftful.Craftful;
@@ -15,13 +16,20 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.superdog.craftful.block.custom.PizzaBlock;
-import net.superdog.craftful.block.custom.RackBlock;
-import net.superdog.craftful.block.custom.TomatoCropBlock;
-import net.superdog.craftful.block.custom.planterBlock;
+import net.superdog.craftful.block.custom.*;
 import net.superdog.craftful.item.ModItemGroup;
 
 public class ModBlocks {
+
+    public static final Block LIMESTONE = registerBlock("limestone",
+            new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).requiresTool()), ModItemGroup.CRAFTFUL);
+
+    public static final Block LIMESTONE_BRICKS = registerBlock("limestone_bricks",
+            new Block(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).requiresTool()), ModItemGroup.CRAFTFUL);
+
+    public static final Block LIMESTONE_PILLAR = registerBlock("limestone_pillar",
+            new LimestonePillarBlock(FabricBlockSettings.of(Material.STONE).strength(1.5F, 6.0F).requiresTool()), ModItemGroup.CRAFTFUL);
+
 
     public static final Block RACK = registerBlock("rack",
             new RackBlock(FabricBlockSettings.of(Material.WOOD).strength(2.0f, 3.0f).sounds(BlockSoundGroup.WOOD)), ModItemGroup.CRAFTFUL);
